15c8424bd7c11c73e2f75093e56f8ad1c0acb7ea
mark-mansfield <markgmansfield@gmail.com>
2019-03-13 12:23:36 +1100

    added simple UI for file selection

--- src/app/dashboard/import-order/import-order.component.ts
+++ src/app/dashboard/import-order/import-order.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { mimeType } from './mime-type.validator';
+import { FormBuilder, FormGroup, FormControl, Validators, RequiredValidator } from '@angular/forms';
 @Component({
   selector: 'app-import-order',
   templateUrl: './import-order.component.html',
   styleUrls: ['./import-order.component.css']
 })
 export class ImportOrderComponent implements OnInit {
+  filePicked = '';
+  disabledUpload = true;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onFilePicked(event) {
+    this.filePicked = `File Chosen: ${event.target.files[0].name}`;
+    this.disabledUpload = false;
   }
-
 }
