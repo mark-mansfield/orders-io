8a08d00ae2def62f3b605fc2fb6d56ed7c6d7db7
mark-mansfield <markgmansfield@gmail.com>
2019-05-09 12:14:06 +1000

    initial commit

---
+++ src/app/dishes/dishes-list/dishes-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-dishes-list',
+  templateUrl: './dishes-list.component.html',
+  styleUrls: ['./dishes-list.component.css']
+})
+export class DishesListComponent implements OnInit {
+  @Input() data: any;
+  @Output() selectItem = new EventEmitter<string>();
+  constructor() {}
+
+  onItemSelect(item) {
+    this.selectItem.emit(item);
+  }
+
+  ngOnInit() {
+    console.log(this.data);
+  }
+}
