2277a6acc21c9ac089236e7eb39eda35393f4c25
mark-mansfield <markgmansfield@gmail.com>
2019-05-12 11:03:24 +1000

    added dish create component

--- src/app/dishes/dishes.module.ts
+++ src/app/dishes/dishes.module.ts
@@ -4,10 +4,11 @@ import { AngularMaterialModule } from '../angular-material';
 import { DishesComponent } from './dishes.component';
 import { DishesListComponent } from './dishes-list/dishes-list.component';
 import { DishDetailsEditComponent } from './dish-details-edit/dish-details-edit.component';
+import { DishCreateComponent } from './dish-create/dish-create.component';
 
 @NgModule({
   imports: [CommonModule, AngularMaterialModule],
-  declarations: [DishesListComponent, DishDetailsEditComponent, DishesComponent],
+  declarations: [DishesListComponent, DishDetailsEditComponent, DishesComponent, DishCreateComponent],
   exports: [DishesListComponent, DishesComponent, DishDetailsEditComponent]
 })
 export class DishesModule {}
