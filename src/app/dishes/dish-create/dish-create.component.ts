e0d60edef8e0bb5f196ed1010cb356a743c57944
mark-mansfield <markgmansfield@gmail.com>
2019-05-12 11:00:55 +1000

    initial commit

---
+++ src/app/dishes/dish-create/dish-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-create',
+  templateUrl: './dish-create.component.html',
+  styleUrls: ['./dish-create.component.css']
+})
+export class DishCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
