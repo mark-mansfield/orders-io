8ef352d46bb694fcdce44d5ecfa8111781432cfd
mark-mansfield <markgmansfield@gmail.com>
2019-05-20 14:33:38 +1000

    added FormsModule and reactiveForms Module to imports array

--- src/app/dishes/dishes.module.ts
+++ src/app/dishes/dishes.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from '../angular-material';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { DishesComponent } from './dishes.component';
 import { DishesListComponent } from './dishes-list/dishes-list.component';
 import { DishDetailsEditComponent } from './dish-details-edit/dish-details-edit.component';
 import { DishCreateComponent } from './dish-create/dish-create.component';
 @NgModule({
-  imports: [CommonModule, AngularMaterialModule],
+  imports: [CommonModule, AngularMaterialModule, ReactiveFormsModule, FormsModule],
   declarations: [DishesListComponent, DishDetailsEditComponent, DishesComponent, DishCreateComponent],
   exports: [DishesListComponent, DishesComponent, DishDetailsEditComponent]
 })
